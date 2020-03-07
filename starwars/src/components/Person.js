9d34185795e10e1aa16ad07b98df9e4fbb9f38f7
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-03-06 21:46:17 -0500

    elements displaying to the screen, questions answered

--- starwars/src/components/Person.js
+++ starwars/src/components/Person.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 function Person (props) {
-
 console.log(props);
+
     return (
     <div className="person">
-      <h1>{props.data}</h1>
+      <h1>{props.name}</h1>
+      <p>{props.height}</p>
+      <p>{props.mass}</p>
+      <p>{props.hairColor}</p>
+      <p>{props.skinColor}</p>
+      <p>{props.eyeColor}</p>
+      <p>{props.birthYear}</p>
+      <p>{props.gender}</p>
+      <p>{props.homeworld}</p>
       
     </div>
 
