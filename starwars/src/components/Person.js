a81d543da55c2f127491f135ed00f1f46e5a3237
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-03-06 21:55:06 -0500

    styled paragraphs

--- starwars/src/components/Person.js
+++ starwars/src/components/Person.js
@@ -1,19 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
 function Person (props) {
 console.log(props);
 
+const Paragraph = styled.p`
+    color: red;
+    border: 5px dotted blue;
+    font-size: 1rem;
+    font-family: Tahoma, Geneva, sans-serif;
+    border-radius: 10px;
+    margin: 2% 10%;
+    padding: 1%;
+`
+
     return (
     <div className="person">
       <h1>{props.name}</h1>
-      <p>{props.height}</p>
-      <p>{props.mass}</p>
-      <p>{props.hairColor}</p>
-      <p>{props.skinColor}</p>
-      <p>{props.eyeColor}</p>
-      <p>{props.birthYear}</p>
-      <p>{props.gender}</p>
-      <p>{props.homeworld}</p>
+      <Paragraph>{props.height}</Paragraph>
+      <Paragraph>{props.mass}</Paragraph>
+      <Paragraph>{props.hairColor}</Paragraph>
+      <Paragraph>{props.skinColor}</Paragraph>
+      <Paragraph>{props.eyeColor}</Paragraph>
+      <Paragraph>{props.birthYear}</Paragraph>
+      <Paragraph>{props.gender}</Paragraph>
+     
       
     </div>
 
